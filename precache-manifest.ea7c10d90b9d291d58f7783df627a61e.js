19fdbb227ba9b21af7d60bd9935a7ae0f83e629e
Anton-Schogolev <Anton.Schogolev@gmail.com>
2020-12-11 17:08:14 +0300

    Updates

---
+++ precache-manifest.ea7c10d90b9d291d58f7783df627a61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec383dcc56eb7c7faa3b6772b1e07818",
+    "url": "/ProgectForReactLearning/index.html"
+  },
+  {
+    "revision": "c46f2d48c05de7b26d35",
+    "url": "/ProgectForReactLearning/static/css/main.1834b0d3.chunk.css"
+  },
+  {
+    "revision": "d09f326bb9046c2ddd90",
+    "url": "/ProgectForReactLearning/static/js/2.48c76a95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProgectForReactLearning/static/js/2.48c76a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46f2d48c05de7b26d35",
+    "url": "/ProgectForReactLearning/static/js/main.822f5ce3.chunk.js"
+  },
+  {
+    "revision": "d88fb86d2041046073c0",
+    "url": "/ProgectForReactLearning/static/js/runtime-main.bd88e229.js"
+  }
+]);

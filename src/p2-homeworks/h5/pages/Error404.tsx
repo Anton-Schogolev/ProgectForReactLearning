63c477cc8ecd3b687246dc90f7fb724550187738
Anton-Schogolev <Anton.Schogolev@gmail.com>
2020-12-11 19:15:35 +0300

    completed 404

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState,MouseEvent} from "react";
+import s from "./Error404.module.css"
 
+type Coordinates = {
+    'top': number
+    'left': number
+}
 function Error404() {
+    const[coordinates,setCoordinates] = useState<Coordinates>({top:0,left:0})
+    function callback(event: MouseEvent<HTMLDivElement>) {
+        setCoordinates({top:event.pageY,left:event.pageX})
+    }
     return (
-        <div>
-            <div>404</div>
+        <div className={s.container} onMouseMove={callback}>
+            {/*<div>404</div>
             <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
+
+            <div className={s.text}>
+                <h1>404</h1>
+                <h2>Uh, Ohh</h2>
+                <h3>Sorry we cant find what you are looking for 'cuz its so dark in here</h3>
+            </div>
+            <div className={s.torch} style={coordinates}/>
         </div>
     );
 }
